2622dc9c7ecb2c8c229906a7f9af42392dda4382
MedvedevMM <Tyuiu.MedvedevMM@outlook.com>
2024-09-16 00:07:00 +0500

    Task1

---
+++ Tyuiu.MedvedevMM.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Матвей";
+        int age = 21;
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " год. ");
+        Console.ReadKey();
+    }
+}
